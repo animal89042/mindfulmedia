8ba333450f96d38e7f69d879a6c5c83a21a9c441
Brody Michaels <brodygmichaels@gmail.com>
2025-07-14 14:23:39 -0400

    Fixed mismatched names. users Table used displayname while init called for display_name. change users Table to use display_name.

--- backend/init.sql
+++ backend/init.sql
@@ -3,7 +3,7 @@ USE mindfulmedia;
 
 CREATE TABLE IF NOT EXISTS users (
   steam_id      VARCHAR(50) PRIMARY KEY,
-  displayname  VARCHAR(255),
+  display_name  VARCHAR(255),
   personaname  VARCHAR(255),
   avatar       VARCHAR(512),
   profile_url  VARCHAR(512)
