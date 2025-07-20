6a9ac24a858c3824636b54fa6c1a758ece9594e3
Brody <brodygmichaels@gmail.com>
2025-07-19 20:34:13 -0400

    Backend: Updated DB to store steam_id, title, created_at, and edited_at in the journals table. Modified all journal-related API routes to use the SteamID from session rather than URL/query parameters as it was getting messy. Added middleware to enforce authentication.
    Frontend: I added a title input for journal entries in GamePage.js and updated journal display to show titles and edit dates. In Journal.js, updated the journal list table to include game titles and journal entry titles. Adjusted API calls to send cookies for authenticated requests. REFACTORED TO USE SESSION SO STEAM ID IS NO LONGER IN URL OR FLOATING AROUND...
    
    This push will probabaly break things and I will be debugging for 100 million years c: Good luck me!
    
    Ill keep adding to this for all debugging:
    
    1st: Fixed missing Uppercase in server.js import of AuthMiddleware
    2nd: Fixed Accidental Deletion of const useState() for given var and deleted SteamID Dependency
    3rd: Fixed Missed redirect from steam login still redirected to /userid rather than /
    4th: Fixed missed rename of id to user.
    5th: Added debug console log to check correct checking of Homepage redirect decision.
    6th: Changed CORS allowed origin to frontend, "*" not allowed when using cookies.
    7th: Added missing cookie components.
    8th: Fixed wrong set value for sameSite cookie component.
    9th: Added debug console log for CORS Blocked.
    10th: Adjusted CORS to allow missing exceptions along with changes to origin function handling.
    11th: Logging every check.
    12th: Setting saveUninitialized to check for cookie problem.
    13th: Set Stateless to False.
    14th: Console log debug.

--- backend/AuthMiddleware.js
+++ backend/AuthMiddleware.js
@@ -1,4 +1,6 @@
 export const requireSteamID = (req, res, next) => {
+    console.log(req.session?.passport?.user?.id)
+    console.log ("USER ID")
     const steam_id = req.session?.passport?.user?.id;
     if (!steam_id) {
         return res.status(401).json({ error: 'Not logged in' });
