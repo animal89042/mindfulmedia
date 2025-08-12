2768c968a31a1caf5debf9cb00ecca3afe1b7f58
g-burks <g.burks153@gmail.com>
2025-08-12 15:08:28 -0400

    Local Dev fixes

--- frontend/src/apiRoutes.js
+++ frontend/src/apiRoutes.js
@@ -1,7 +1,15 @@
-const BASE = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_BASE_URL : "/api";
+const BASE = process.env.REACT_APP_API_BASE_URL;
+
+// Login must be full backend URL in dev
+let LOGIN_BASE;
+if (process.env.NODE_ENV === "production") {
+    LOGIN_BASE = BASE;
+} else {
+    LOGIN_BASE = "http://localhost:5000/api";
+}
 
 const apiRoutes = {
-    login: `${BASE}/auth/steam/login`,
+    login: `${LOGIN_BASE}/auth/steam/login`,
     getUser: `${BASE}/me`,
     getGame: (appid) => `${BASE}/game/${appid}`, //fetch game details
     getGames:  `${BASE}/games`,
