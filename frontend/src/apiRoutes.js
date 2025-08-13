90f8740c1366cb250ac1a7bb7d1638ebf03ae0f2
Gentry <g.burks153@gmail.com>
2025-08-13 15:21:53 -0400

    Update apiRoutes.js

--- frontend/src/apiRoutes.js
+++ frontend/src/apiRoutes.js
@@ -1,4 +1,4 @@
-const BASE = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
+const BASE = process.env.REACT_APP_API_BASE_URL || "/api";
 
 const apiRoutes = {
     login: `${BASE}/auth/steam/login`,
