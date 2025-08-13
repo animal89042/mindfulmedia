f2fcc5867f1ca9cf7c75c19bfdd6bc0d4c9bacff
g-burks <g.burks153@gmail.com>
2025-08-13 14:41:01 -0400

    - Fixed issue where backend wasn't included in cors allowed origin list - Reverted api logic to use relative '/api' pathing in production rather than having it hardcoded - Added frontend./env

--- frontend/src/apiRoutes.js
+++ frontend/src/apiRoutes.js
@@ -1,7 +1,4 @@
-const BASE =
-    process.env.NODE_ENV === "production"
-        ? "/api"
-        : "http://localhost:5000/api";
+const BASE = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
 
 const apiRoutes = {
     login: `${BASE}/auth/steam/login`,
